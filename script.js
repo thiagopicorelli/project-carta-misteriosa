201ae2a78c3359a3317941ef6f03e332d90f45ca
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 17:53:33 -0300

    Adiciona aviso para não deixar o input vazio

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const criarButton = document.getElementById('criar-carta');
 const cartaGerada = document.getElementById('carta-gerada');
 
 function gerarCarta() {
-  console.log('test');
-  let textoDividido = cartaTexto.value.split(' ');
+  if(cartaTexto.value.trim().length === 0) {
+    cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    return;
+  }
 
+  let textoDividido = cartaTexto.value.split(' ');
   cartaGerada.innerHTML = '';
   for(let i = 0; i < textoDividido.length; i += 1) {
     const span = document.createElement('span');

21af15335deb4a87e3cb46800a91dfc44fd4026f
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 17:44:00 -0300

    Adiciona texto dividido em spans no output

---
+++ script.js
@@ -0,0 +1,16 @@
+const cartaTexto = document.getElementById('carta-texto');
+const criarButton = document.getElementById('criar-carta');
+const cartaGerada = document.getElementById('carta-gerada');
+
+function gerarCarta() {
+  console.log('test');
+  let textoDividido = cartaTexto.value.split(' ');
+
+  for(let i = 0; i < textoDividido.length; i += 1) {
+    const span = document.createElement('span');
+    span.innerHTML = textoDividido[i];
+    cartaGerada.appendChild(span);
+  }
+}
+
+criarButton.addEventListener('click', gerarCarta);

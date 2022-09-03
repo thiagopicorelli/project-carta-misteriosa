05aa1433736f99ff9a1fd087d2d8dd2cca8054b6
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-03 13:58:16 -0300

    Modifica gerador de classes para adicionar 1 classe de cada estilo, e corrige codigo js para o lint

--- script.js
+++ script.js
@@ -8,70 +8,42 @@ const estilos = [
   ['medium', 'big', 'reallybig'],
   ['rotateleft', 'rotateright'],
   ['skewleft', 'skewright'],
-]
+];
 
 function gerarEstilo() {
-  let ordem = {};
   let classes = '';
 
-  const primeiroEstilo = Math.floor(Math.random() * 4);
-  ordem[primeiroEstilo] = true;
-  if (primeiroEstilo === 0) { // Para garantir que haja no mínimo dois estilos
-    ordem[primeiroEstilo + 1] = true;
-  } else {
-    ordem[primeiroEstilo - 1] = true;
-  }
-
-  for (let i = 0; i < 2; i += 1) {
-    ordem[Math.floor(Math.random() * 4)] = true;
-  }
-
-  for (const key of Object.keys(ordem)) {
-    if(isNaN(key)) {
-      continue;
-    }
-
-    const keyn = parseInt(key);
-    
-    classes += estilos[keyn][Math.floor(Math.random() * estilos[keyn].length)] + ' ';
+  for (let i = 0; i < 4; i += 1) {
+    classes += `${estilos[i][Math.floor(Math.random() * estilos[i].length)]} `;
   }
 
   return classes.trim();
 }
 
-function gerarEstiloDiferente() {
-  let classes = '';
-
-  for (let i = 0; i < 4; i += 1) {
-    classes += estilos[i][Math.floor(Math.random() * estilos[i].length)] + ' ';
-  }
-
-  return classes;
-}
-
-function addEstiloDiferente(event) {
+function addEstilo(event) {
   let classes;
+  const { target } = event;
 
   do {
-    classes = gerarEstiloDiferente();
+    classes = gerarEstilo();
   } while (classes === event.target.className);
 
-  event.target.className = classes;
+  target.className = classes;
 }
 
 function gerarCarta() {
-  if(cartaTexto.value.trim().length === 0) {
+  if (cartaTexto.value.trim().length === 0) {
     cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
     return;
   }
 
-  let textoDividido = cartaTexto.value.split(' ');
+  const textoDividido = cartaTexto.value.trim().split(' ');
   cartaGerada.innerHTML = '';
-  for(let i = 0; i < textoDividido.length; i += 1) {
+  for (let i = 0; i < textoDividido.length; i += 1) {
     const span = document.createElement('span');
     span.innerHTML = textoDividido[i];
     span.className = gerarEstilo();
-    span.addEventListener('click', addEstiloDiferente);
+    span.addEventListener('click', addEstilo);
     cartaGerada.appendChild(span);
   }
 
